a6bc678ecb0f53e2e00bbbebca26168020f55482
RocketMarvin <jonkermarvin1@gmail.com>
2024-05-15 22:07:32 +0200

    Separate axises fix
    car controller is nog niet helemaal af maar kom in de buurt

--- RaceGameDeepdive/Assets/Scripts/Marvin/WheelController.cs
+++ RaceGameDeepdive/Assets/Scripts/Marvin/WheelController.cs
@@ -12,27 +12,63 @@ public class WheelController : MonoBehaviour
     [SerializeField] private Transform rearLeftTransform, rearRightTransform;
 
     public float acceleration = 1000f;
-    public float backAcceleration = 300f;
+    public float backAcceleration = -300f;
     public float brakeForce = 300f;
     public float maxTurnAngle = 45f;
 
+    public Rigidbody rb;
+
     [SerializeField] private float currentAcceleration = 0f;
     [SerializeField] private float currentBackAcceleration = 0f;
+    [SerializeField] private float currentSpeed;
     [SerializeField] private float currentBrakeForce = 0f;
     [SerializeField] private float currentTurnAngle = 0f;
 
-    private void FixedUpdate()
+    [SerializeField]private bool isInReverse = false, isStandingStill = false;
+
+    private void Update()
     {
-        currentAcceleration = acceleration * Input.GetAxis("RTTrigger");
-        currentBackAcceleration = backAcceleration * Input.GetAxis("LTTrigger");
+        currentSpeed = rb.velocity.magnitude * 3.6f;
+
+        if (Input.GetAxis("LTTrigger") != 0) currentBrakeForce = brakeForce;
+        else currentBrakeForce = 0;
+
+        if (!isInReverse)
+        {
+            currentAcceleration = acceleration * Input.GetAxis("RTTrigger");
+            currentAcceleration = -currentBrakeForce * Input.GetAxis("LTTrigger");
+        }
+        else
+        {
+            currentAcceleration = -acceleration * Input.GetAxis("RTTrigger");
+            currentAcceleration = currentBrakeForce * Input.GetAxis("LTTrigger");
+        }
+
+        if (currentSpeed != 0) isStandingStill = false;
+        else
+        {
+            isStandingStill = true;
+            currentAcceleration = 0;
+        }
+
+        rearRightWheelCollider.motorTorque = currentAcceleration;
+        rearLeftWheelCollider.motorTorque = currentAcceleration;
+
         Debug.Log("forward: " + currentAcceleration);
-        Debug.Log("backward: " + currentBackAcceleration);
+        Debug.Log("backward: " + currentAcceleration);
+        Debug.Log("brake: " + currentBrakeForce);
 
         if (Input.GetKey(KeyCode.Space)) currentBrakeForce = brakeForce;
         else currentBrakeForce = 0;
 
-        rearRightWheelCollider.motorTorque = currentAcceleration;
-        rearLeftWheelCollider.motorTorque = currentAcceleration;
+        if (isStandingStill)
+        {
+            if (Input.GetKeyDown(KeyCode.JoystickButton1))
+            {
+                isInReverse = !isInReverse;
+                print("isInReverse: " + isInReverse);
+            }
+        }
 
         frontRightWheelCollider.brakeTorque = currentBrakeForce;
         frontLeftWheelCollider.brakeTorque = currentBrakeForce;
